9ed35c687a6e305fca35f3949fd176ed5c88e128
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-08-25 16:26:05 +0300

    4.25 done

---
+++ Part4/4.25_PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -0,0 +1,29 @@
+package main.java;
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class PrintingASpecifiedFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which file should have its contents printed?");
+        String fileName = scanner.nextLine();
+
+        printContent(fileName);
+
+    }
+
+    static void printContent(String filename) {
+        try (Scanner scanner = new Scanner(Paths.get(filename))) {
+
+            while (scanner.hasNextLine()) {
+                String row = scanner.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
