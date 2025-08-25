d284b3da4356c35a53da0b7e22fcae60175b7c06
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-08-25 15:50:57 +0300

    4.22 done

---
+++ Part4/4.22_Cubes/src/main/java/Cubes.java
@@ -0,0 +1,26 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class Cubes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            String row = scanner.nextLine();
+
+            if (row.equals("end")) {
+                break;
+            }
+
+            int number = Integer.valueOf(row);
+            System.out.println(cube(number));
+        }
+    }
+
+    static int cube(int num) {
+        return num * num * num;
+    }
+}
