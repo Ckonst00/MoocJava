786b70490819b91777984523c233f75d829310bf
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-08-25 19:40:17 +0300

    4.29 done

---
+++ Part4/4.29_RecordsFromAFile/README.md
@@ -0,0 +1,30 @@
+## Excercise description
+
+write a program that first prompts the user for the name of the file they want to read. The program then prints the content of the file in the following way
+
+---
+Sample data:
+
+lily,3
+
+anton,5
+
+levi,4
+
+amy,1
+
+---
+Sample output:
+
+Name of the file:
+
+**data.txt**
+
+
+lily, age: 3 years
+
+anton, age: 5 years
+
+levi, age: 4 years
+
+amy, age: 1 year
---
+++ Part4/4.29_RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -0,0 +1,29 @@
+package main.java;
+
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class RecordsFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file: ");
+        String file = scanner.nextLine();
+
+        try (Scanner scanner1 = new Scanner(Paths.get(file))) {
+            while (scanner1.hasNextLine()) {
+                String line = scanner1.nextLine();
+
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+                System.out.println(name + ", " + "age: " + age + " years");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
