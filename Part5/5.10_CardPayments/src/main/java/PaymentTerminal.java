c1476c819d339b72807b3db7dfdc996273507e48
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-08-30 17:35:33 +0300

    5.10 part 2 done

--- Part5/5.10_CardPayments/src/main/java/PaymentTerminal.java
+++ Part5/5.10_CardPayments/src/main/java/PaymentTerminal.java
@@ -1,33 +1,42 @@
-
 public class PaymentTerminal {
-
-
     private double money;  // amount of cash
     private int affordableMeals; // number of sold affordable meals
     private int heartyMeals;  // number of sold hearty meals
 
-
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the change
+        // increase the amount of cash by the price of an affordable meal and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment < 2.5) {
+            return payment;
+        } else {
+            this.money += 2.50;
+            this.affordableMeals++;
+            return payment - 2.50;
+        }
+
+
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
-        // increase the amount of cash by the price of a hearty mean and return the change
+        // increase the amount of cash by the price of a hearty meal and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment < 4.3) {
+            return payment;
+        } else {
+            this.money += 4.30;
+            this.heartyMeals++;
+            return payment - 4.30;
+        }
     }
 
-
-    @Override
     public String toString() {
-        return "money: " + money + ", number of sold affordable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+        return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
     }
 }
