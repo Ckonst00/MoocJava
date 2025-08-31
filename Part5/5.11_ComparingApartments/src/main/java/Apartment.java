cdfdecf6d354b6b9d33e6e160e9f9443bb91bda6
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-08-31 13:47:28 +0300

    5.11 part2 done

--- Part5/5.11_ComparingApartments/src/main/java/Apartment.java
+++ Part5/5.11_ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,12 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
     }
 
     public Boolean largerThan(Apartment compared) {
@@ -19,4 +19,9 @@ public class Apartment {
         }
     }
 
+    public int priceDifference(Apartment compared) {
+        int price1 = this.pricePerSquare * this.squares;
+        int price2 = compared.pricePerSquare * compared.squares;
+        return Math.abs(price1 - price2);
+    }
 }
