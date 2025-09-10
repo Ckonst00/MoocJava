10c0e68820ff7d3b2be1525a89bfeac772f3970d
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-09-10 12:19:56 +0300

    6.9 done

---
+++ Part6/6.9_SimpleDictionary/src/main/java/TextUI.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+public class TextUI {
+
+    private Scanner scanner;
+    private SimpleDictionary dictionary;
+
+    public TextUI(Scanner scanner, SimpleDictionary dictionary) {
+        this.scanner = scanner;
+        this.dictionary = dictionary;
+    }
+
+    public void start() {
+
+        while (true) {
+
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("end")) {
+                System.out.println("Bye bye!");
+                break;
+
+            } else if (command.equals("add")) {
+                System.out.print("Word: ");
+                String word = scanner.nextLine();
+
+                System.out.print("Translation: ");
+                String translation = scanner.nextLine();
+
+                this.dictionary.add(word, translation);
+
+            } else if (command.equals("search")) {
+
+                System.out.print("To be translated: ");
+                String seachWord = scanner.nextLine();
+
+                String found = this.dictionary.translate(seachWord);
+                if (found == null) {
+                    System.out.println("Word " + seachWord + " was not found");
+
+                } else {
+
+                    System.out.println("Translation: " + found);
+                }
+
+            } else {
+
+                System.out.println("Unknown command");
+            }
+        }
+
+    }
+}
